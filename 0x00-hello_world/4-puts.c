53d06bf3d486d55af856e0f298220f9d09e03005
Abdallah Afsa <Abdallahafsa@yahoo.com>
2023-02-20 09:10:22 -0800

    4.Hello, Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,8 @@
  * main - printing a sentence using puts func
  * program should end with 0
  */
-int main(void){
+int main(void)
+{
 puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }
