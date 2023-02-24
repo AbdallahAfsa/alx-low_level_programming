05c76f7a200b2707a749d33e65fad419bb20fca5
Abdallah Afsa <Abdallahafsa@yahoo.com>
2023-02-23 17:11:17 -0800

    0. Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * description: test func that print if int is + or, -
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
